55e76e4ef9e46768d3d1d6d752bc4941340873fb
Nikita Rudakov <boltgopnik@gmail.com>
2024-09-28 09:57:58 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import telebot, constants
+
+bot = telebot.TeleBot(token=constants.TOKEN, parse_mode="Markdown")
+
+
+@bot.message_handler()
+def message_handler(message):
+    text = message.text
+    in_whitelist = message.from_user.id in constants.WHITELIST
+
+    if in_whitelist:
+        if text == "/start":
+            bot.send_message(message.from_user.id, "*Hello!*")
+
+
+bot.polling()
